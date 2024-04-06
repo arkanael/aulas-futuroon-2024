4abd7f4df4b8a28e581ba0eb95827829d57cc91e
Luiz Guilherme Bandeira <arkanael@gmail.com>
2024-04-06 14:41:26 -0300

    Conceito de variavel

---
+++ Introdução ao CSharp/projeto01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; //Pacotes (modulos) da aplicação  
+
+namespace Projeto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.WriteLine("Olá, meu nome é Guilherme!");
+        }
+    }
+}
